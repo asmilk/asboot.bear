dec7dc351e7b79d96b17e2f9e3374836b24f9cc5
asmilk <asmilk@sina.com>
2023-05-20 19:31:53 +0800

    default

---
+++ oauth2-authorization/src/main/java/asboot/auth/OAuth2AuthorizationApplication.java
@@ -0,0 +1,13 @@
+package asboot.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2AuthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuth2AuthorizationApplication.class, args);
+	}
+
+}
